355f4413576c7bec10e43ed3eaa029a697f8e47e
IzzuddinAsroff <izzuddinasroff@gmail.com>
2023-03-12 00:13:48 +0800

    improve logic CRUD

--- Routes/customer.js
+++ Routes/customer.js
@@ -15,24 +15,40 @@ const getCustomerData = () => {
     return JSON.parse(jsonData)
 }
 
+const checkCustomerExist = (uniqId) => {
+    let customersList = getCustomerData()
+    let existCustomer = customersList.find(el => {
+        return el.id == uniqId
+    })
+
+    if (existCustomer == undefined) {
+        return false
+    } else {
+        return true
+    }
+}
+
 
 // add customer
 customerRoutes.post('/customer/add', (req, res) => {
+    try {
+        let customersList = getCustomerData()
+        const id = (Math.random() + 1).toString(36).substring(2);
 
-    let customersList = getCustomerData()
-    const id = (Math.random() + 1).toString(36).substring(2);
-
-    let payload = req.body.data
-    payload.id = id
-    customersList.push(payload)
-    saveCustomerData(customersList);
-    res.send({ success: true, msg: 'customer data added successfully' })
+        let payload = req.body.data
+        payload.id = id
+        customersList.push(payload)
+        saveCustomerData(customersList);
+        res.send({ success: true, msg: 'customer data added successfully' })
+    } catch (error) {
+        res.send({ success: false, msg: `Error has occur`, error: error })
+    }
 })
 
 // get - retrieve all customer list
 customerRoutes.get('/customer/list', (req, res) => {
-    const customersList = getCustomerData()
-    fs.readFile(dataPath, 'utf8', (err, data) => {
+    try {
+        const customersList = getCustomerData()
         let arr = []
         customersList.forEach(el => {
             let objData = {
@@ -46,63 +62,86 @@ customerRoutes.get('/customer/list', (req, res) => {
         });
 
         res.send({ success: true, msg: 'customer list retrieve successfully', data: arr })
-    }, true);
-
+    } catch (error) {
+        res.send({ success: false, msg: `Error has occur`, error: error })
+    }
 })
 
 // get - retrieve specifc customer detail
 customerRoutes.get('/customer/detail', (req, res) => {
-    let customersList = getCustomerData()
-    const uniqId = req.query.id;
-    let existCustomer = customersList.find(el => {
-        return el.id == uniqId
-    })
+    try {
+        let customersList = getCustomerData()
+        const uniqId = req.query.id;
 
-    res.send({ success: true, msg: 'customer detail retrieve successfully', data: existCustomer })
+        let isCustExit = checkCustomerExist(uniqId)
+        if (!isCustExit) {
+            res.send({ success: false, msg: `customer does not exist` })
+            return
+        }
 
+        let existCustomer = customersList.find(el => {
+            return el.id == uniqId
+        })
+
+        res.send({ success: true, msg: 'customer detail retrieve successfully', data: existCustomer })
+    } catch (error) {
+        res.send({ success: false, msg: `Error has occur`, error: error })
+    }
 });
 
 // put - retrieve specifc customer detail
 customerRoutes.put('/customer/edit', (req, res) => {
-    let customersList = getCustomerData()
+    try {
+        let customersList = getCustomerData()
 
-    const requestBody = req.body.data
-    const uniqId = requestBody.id;
+        const requestBody = req.body.data
+        const uniqId = requestBody.id;
 
-    let existCustomer = customersList.find(el => {
-        return el.id == uniqId
-    })
-
-    let notSelectedCustomer = customersList.find(el => {
-        return el.id != uniqId
-    })
-    if (requestBody.personalData !== undefined) {
-        for (const key in requestBody.personalData) {
-            existCustomer.personalData[key] = requestBody.personalData[key]
+        let isCustExit = checkCustomerExist(uniqId)
+        if (!isCustExit) {
+            res.send({ success: false, msg: `customer does not exist` })
+            return
         }
-    }
 
-    if (requestBody.maritalData !== undefined) {
-        for (const key in requestBody.maritalData) {
-            existCustomer.maritalData[key] = requestBody.maritalData[key]
+        let existCustomer = customersList.find(el => {
+            return el.id == uniqId
+        })
+
+        let notSelectedCustomer = customersList.filter(el => {
+            return el.id != uniqId
+        })
+
+        if (requestBody.personalData !== undefined) {
+            for (const key in requestBody.personalData) {
+                existCustomer.personalData[key] = requestBody.personalData[key]
+            }
         }
-    }
 
-    if (requestBody.emergencyContact !== undefined) {
-        for (const key in requestBody.emergencyContact) {
-            existCustomer.emergencyContact[key] = requestBody.emergencyContact[key]
+        if (requestBody.maritalData !== undefined) {
+            for (const key in requestBody.maritalData) {
+                existCustomer.maritalData[key] = requestBody.maritalData[key]
+            }
         }
+
+        if (requestBody.emergencyContact !== undefined) {
+            for (const key in requestBody.emergencyContact) {
+                existCustomer.emergencyContact[key] = requestBody.emergencyContact[key]
+            }
+        }
+
+        let arr = []
+        if (notSelectedCustomer.length > 0) {
+            arr.push(notSelectedCustomer)
+        }
+
+        arr.push(existCustomer)
+
+        saveCustomerData(arr);
+        res.send({ success: true, msg: `customer ${existCustomer.personalData.identificationNo} updated successfully` })
+    } catch (error) {
+        res.send({ success: false, msg: `Error has occur`, error: error })
     }
 
-    let arr = []
-    if (notSelectedCustomer != null) {
-        arr.push(notSelectedCustomer)
-    }
-
-    arr.push(existCustomer)
-
-    saveCustomerData(arr);
-    res.send({ success: true, msg: `customer ${existCustomer.personalData.identificationNo} updated successfully` })
 
 });
 
@@ -114,19 +153,24 @@ customerRoutes.post('/customer/delete', (req, res) => {
         console.log('uniqId', uniqId);
 
         if (uniqId != undefined) {
+            let isCustExit = checkCustomerExist(uniqId)
+            if (!isCustExit) {
+                res.send({ success: false, msg: `customer does not exist` })
+                return
+            }
+
             let customersList = getCustomerData()
             let existCustomers = customersList.filter(el => {
                 return el.id != uniqId
             })
-    
-    
+
             saveCustomerData(existCustomers);
-            res.send({ success: true, msg: `Customer with id ${bodyData.identificationNo} has been deleted` })
+            res.send({ success: true, msg: `Customer with id ${bodyData.personalData.identificationNo} has been deleted` })
         } else {
-            res.send({ success: false, msg: `Customer with id ${bodyData.identificationNo} not found` })
+            res.send({ success: false, msg: `Customer with id ${bodyData.personalData.identificationNo} not found` })
         }
     } catch (error) {
-        res.send({ success: false, msg: `Customer with id ${bodyData.identificationNo} fail to delete` })
+        res.send({ success: false, msg: `Error has occur`, error: error })
     }
 
 })
